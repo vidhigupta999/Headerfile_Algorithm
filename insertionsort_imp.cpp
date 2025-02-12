ecc5ab11a2936c04a9c849f06ac449fce7887497
vidhi gupta <160422955+vidhigupta999@users.noreply.github.com>
2025-02-12 23:38:05 +0530

    Add files via upload

---
+++ insertionsort_imp.cpp
@@ -0,0 +1,19 @@
+#include "algorithm.h"
+
+// Insertion Sort function implementation
+template<typename T>
+void insertion_sort(std::vector<T>& arr) {
+    int n = arr.size();
+    for (int i = 1; i < n; ++i) {
+        T key = arr[i];
+        int j = i - 1;
+
+        // Move elements of arr[0..i-1], that are greater than key,
+        // to one position ahead of their current position
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            --j;
+        }
+        arr[j + 1] = key;
+    }
+}
